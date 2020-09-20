2ad47bb067cca14348bb61dbb131f4fcccb91d1b
TETRA2000 <sagittariusm25@gmail.com>
2020-09-20 11:43:00 +0900

    Initial Commit

---
+++ DNS Configurator/DNS_ConfiguratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DNS_ConfiguratorApp.swift
+//  DNS Configurator
+//
+//  Created by Takahiko Inayama on 2020/09/20.
+//
+
+import SwiftUI
+
+@main
+struct DNS_ConfiguratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DNS ConfiguratorTests/DNS_ConfiguratorTests.swift
@@ -0,0 +1,33 @@
+//
+//  DNS_ConfiguratorTests.swift
+//  DNS ConfiguratorTests
+//
+//  Created by Takahiko Inayama on 2020/09/20.
+//
+
+import XCTest
+@testable import DNS_Configurator
+
+class DNS_ConfiguratorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
