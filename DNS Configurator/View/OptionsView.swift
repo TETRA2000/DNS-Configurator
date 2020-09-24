992e2e5fc88b6898b7a21bf0b6de5ff467297aa5
TETRA2000 <sagittariusm25@gmail.com>
2020-09-25 00:41:37 +0900

    Enhance Option Screen

--- DNS Configurator/View/OptionsView.swift
+++ DNS Configurator/View/OptionsView.swift
@@ -10,42 +10,73 @@ import NetworkExtension
 
 struct OptionsView: View {
     @State var showAlert = false
+    @State var showRemovalAlert = false {
+        didSet {
+            showAlert = showRemovalAlert || showActiveAlert
+        }
+    }
+    @State var showActiveAlert = false {
+        didSet {
+            showAlert = showRemovalAlert || showActiveAlert
+        }
+    }
     @EnvironmentObject var dnsSettings: DNSSettings
     
     var body: some View {
         VStack {
-            if (dnsSettings.active != nil) {
-                Button(action: {removeDoH()}) {
-                    Text("Remove DNS resolver setting")
-                        .foregroundColor(Color.red)
+            Text("Extras")
+                .font(.headline)
+            List {
+                Section(header: Text("Options")) {
+                    if (dnsSettings.active != nil) {
+                        Button(action: {validateToRemoveDoH()}) {
+                            Text("Remove the resolver setting")
+                                .foregroundColor(Color.red)
+                        }
+                        .alert(isPresented: $showAlert) {
+                            if (showActiveAlert) {
+                                return Alert(
+                                    title: Text("Can't remove active setting"),
+                                    message: Text("Please select another DNS provider in Setting app.")
+                                )
+                            } else {
+                                return Alert(
+                                    title: Text("Confirmation"),
+                                    message: Text("Are you sure to remove the resolver setting?"),
+                                    primaryButton: .destructive(Text("Remove"), action: {dnsSettings.removeDoH()}),
+                                    secondaryButton: .cancel(Text("Cancel"), action: {showRemovalAlert.toggle()})
+                                )
+                            }
+                        }
+           
+                    } else {
+                        Text("No options available.")
+                            .foregroundColor(Color.gray)
+                    }
                 }
-                .disabled(dnsSettings.active == nil)
-                .alert(isPresented: $showAlert) {
-                    Alert(
-                        title: Text("Can't remove active setting"),
-                        message: Text("Please select another DNS provider in Setting app.")
-                    )
+                
+                Section(header: Text("About This App")) {
+                    Button(action: {
+                        UIApplication.shared.open(URL(string: "https://github.com/TETRA2000/DNS-Configurator")!)
+                    }, label: {
+                        Text("Source Code (github.com)")
+                    })
                 }
-            } else {
-                Text("No option available.")
-                    .foregroundColor(Color.gray)
             }
         }
     }
 
-    func removeDoH() {
+    func validateToRemoveDoH() {
         NEDNSSettingsManager.shared().loadFromPreferences { loadError in
             if let loadError = loadError {
                 print(loadError)
                 return
             }
             
-            if NEDNSSettingsManager.shared().isEnabled {
-                showAlert = true
-                return
+            showActiveAlert = NEDNSSettingsManager.shared().isEnabled
+            if !showActiveAlert {
+                showRemovalAlert = true
             }
-            
-            dnsSettings.removeDoH()
         }
     }
 }
